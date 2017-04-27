c59715ed22f72b43ff7811853f36148a8fd51fc0
Charles <zhangchaoxu@gmail.com>
2017-04-27 16:54:08 +0800

    抽象grid view

---
+++ base/src/main/java/com/idogfooding/backbone/network/FieldsUtils.java
@@ -0,0 +1,17 @@
+package com.idogfooding.backbone.network;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * FieldsUtils
+ *
+ * @author Charles
+ */
+public class FieldsUtils {
+
+    public static Map<String, Object> getEmptyFields() {
+        return new HashMap<>();
+    }
+
+}
