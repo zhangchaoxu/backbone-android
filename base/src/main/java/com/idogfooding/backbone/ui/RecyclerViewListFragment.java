d1cf3fefb5e931e570b2973d979ec2191657aa5d
Charles <zhangchaoxu@gmail.com>
2017-05-10 09:04:19 +0800

    replace recyclerview fragment

--- base/src/main/java/com/idogfooding/backbone/ui/RecyclerViewListFragment.java
+++ base/src/main/java/com/idogfooding/backbone/ui/RecyclerViewListFragment.java
@@ -2,7 +2,9 @@ package com.idogfooding.backbone.ui;
 
 import android.os.Bundle;
 import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.OrientationHelper;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 
@@ -91,6 +93,12 @@ public abstract class RecyclerViewListFragment<T, A extends BaseQuickAdapter<T, 
 
     protected RecyclerView.LayoutManager getLayoutManager() {
         return new LinearLayoutManager(getContext());
+    }
+
+    protected RecyclerView.LayoutManager getGridLayoutManager(int spanCount) {
+        GridLayoutManager manager = new GridLayoutManager(getContext(), spanCount);
+        manager.setOrientation(OrientationHelper.VERTICAL);
+        return manager;
     }
 
     protected int getLoadAnimation() {
