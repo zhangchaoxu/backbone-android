90a7df955a500aa5e199bb009addd8e2547f639b
Charles <zhangchaoxu@gmail.com>
2017-06-12 17:00:15 +0800

    browser add null check

--- base/src/main/java/com/idogfooding/backbone/utils/DateFormatUtils.java
+++ base/src/main/java/com/idogfooding/backbone/utils/DateFormatUtils.java
@@ -51,4 +51,37 @@ public class DateFormatUtils {
     public static CharSequence formatDate(String format, Date date) {
         return null == date ? "" : DateFormat.format(format, date);
     }
+
+    public static String secondToTime(long time) {
+        String timeStr;
+        long hour;
+        long minute;
+        long second;
+        if (time <= 0)
+            return "00:00";
+        else {
+            minute = time / 60;
+            if (minute < 60) {
+                second = time % 60;
+                timeStr = unitFormat(minute) + ":" + unitFormat(second);
+            } else {
+                hour = minute / 60;
+                if (hour > 99)
+                    return "99:59:59";
+                minute = minute % 60;
+                second = time - hour * 3600 - minute * 60;
+                timeStr = unitFormat(hour) + ":" + unitFormat(minute) + ":" + unitFormat(second);
+            }
+        }
+        return timeStr;
+    }
+
+    public static String unitFormat(long i) {
+        String retStr;
+        if (i >= 0 && i < 10)
+            retStr = "0" + Long.toString(i);
+        else
+            retStr = "" + i;
+        return retStr;
+    }
 }
