121606c571c66070692ccae8767510be2b700cf9
Charles <zhangchaoxu@gmail.com>
2018-06-21 17:12:41 +0800

    upgrade version

--- base2/src/main/java/com/idogfooding/backbone/route/BaseRouteInterceptor.java
+++ base2/src/main/java/com/idogfooding/backbone/route/BaseRouteInterceptor.java
@@ -2,6 +2,7 @@ package com.idogfooding.backbone.route;
 
 import android.support.annotation.NonNull;
 
+import com.chenenyu.router.IRouter;
 import com.chenenyu.router.RouteInterceptor;
 import com.chenenyu.router.RouteResponse;
 
@@ -15,6 +16,21 @@ public class BaseRouteInterceptor implements RouteInterceptor {
     @NonNull
     @Override
     public RouteResponse intercept(Chain chain) {
-        return null;
+        return chain.process();
+    }
+
+    /**
+     * 跳转
+     * @param iRouter
+     * @param chain
+     */
+    protected void routerGo(IRouter iRouter, Chain chain) {
+        if (chain.getFragmentV4() != null) {
+            iRouter.go(chain.getFragmentV4());
+        } else if (chain.getFragment() != null) {
+            iRouter.go(chain.getFragment());
+        } else {
+            iRouter.go(chain.getContext());
+        }
     }
 }
