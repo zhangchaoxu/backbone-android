2d1560d5bca49636db2a6a4f2f4df792efa39fd7
Charles <zhangchaoxu@gmail.com>
2019-05-23 11:41:16 +0800

    升级dialog版本

--- base2/src/main/java/com/idogfooding/backbone/ui/BaseActivity.java
+++ base2/src/main/java/com/idogfooding/backbone/ui/BaseActivity.java
@@ -15,7 +15,6 @@ import android.support.annotation.StringRes;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.content.ContextCompat;
-import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
 import android.view.View;
